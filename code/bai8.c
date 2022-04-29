40e3ecb8fc9be7d6df0d6bbb64f55cc3ac988b04
Nguyễn Thành <nguyenthanh.5s4d@xf164.onmicrosoft.com>
2022-04-29 16:03:33 +0700

    Update bai8.c

--- code/bai8.c
+++ code/bai8.c
@@ -7,7 +7,7 @@ int main () {
   double a = 0; // biến a kiểu double
   scanf ("%d", &n); // nhập vào n
   for (int i = 0; i <= n; i++) { // chạy từ 0 > n
-    a += (double) ((2 * i) + 1) / ((2 * i) + 2); // cộng dồn (2n+1)/(2n+2) vào a, (double) là ép kiểu sang số thực
+    a += 1.0 * (2 * i + 1) / (2 * i + 2); // cộng dồn (2n+1)/(2n+2) vào a
   }
   printf ("%lf ", a); // in ra a
   return 0; // kết thúc
