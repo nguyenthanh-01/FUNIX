ac1a7fd690952fed3566fcda877bc52cbee9936a
Nguyễn Thành <nguyenthanh.5s4d@xf164.onmicrosoft.com>
2022-04-29 15:52:54 +0700

    Create bai7.c

---
+++ code/bai7.c
@@ -0,0 +1,14 @@
+// Tính S(n) = 1/2 + 2/3 + 3/4 + ... + n/(n+1)
+
+#include <stdio.h>
+
+int main () {
+  int n; // biến n kiểu int
+  double a = 0; // biến a kiểu double
+  scanf ("%d", &n); // nhập vào n
+  for (int i = 1; i <= n; i++) { // chạy từ 1 > n
+    a += i * 1.0 / (i + 1); // cộng dồn n/(n+1) vào a
+  }
+  printf ("%lf ", a); // in ra a
+  return 0; // kết thúc
+}
