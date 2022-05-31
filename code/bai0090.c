23e81531702d07caa170f03cc6d63a8110002eee
Nguyễn Thành <nguyenthanh.5s4d@xf164.onmicrosoft.com>
2022-06-01 00:22:00 +0700

    Create bai0090.c

---
+++ code/bai0090.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int N, m, S;
+    printf("\nNhap N: ");
+    scanf("%d", &N);
+
+    S = 0;
+    m = 0;
+    do
+    {
+        m = m + 1;
+        S = S + m;
+    }while(S + m + 1 < N);
+    printf("\nSo nguyen duong m la %d", m);
+    getch();
+    return 0;
+}
