56bd05d5db57f0f1cd6f2a22eea28fe13e8d1d45
Nguyễn Thành <nguyenthanh.5s4d@xf164.onmicrosoft.com>
2022-06-02 02:11:10 +0700

    Create bai0115.c

---
+++ chuong-4/bai0115.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+char hoten [30];
+int toan;
+int van;
+float DiemTrungBinh;
+
+void Nhap()
+{
+    fflush(stdin);
+    printf("\nNhap ho ten: ");
+    gets(hoten);
+
+    printf("\nNhap toan: ");
+    scanf("%d", &toan);
+
+    printf("\nNhap van: ");
+    scanf("%d", &van);
+}
+
+void XuLy()
+{
+    DiemTrungBinh = (toan + van) / 2.0;
+}
+
+void Xuat()
+{
+    printf("\nHo ten: %s", hoten);
+    printf("\nToan: %d", toan);
+    printf("\nVan: %d", van);
+    printf("\nDiem Trung Binh: %f", DiemTrungBinh);
+}
+int main()
+{
+    Nhap();
+    XuLy();
+    Xuat();
+
+    getch();
+    return 0;
+}
