e0bfd2333e0245034e8f97461f9bee97c4fb1811
Nguyễn Thành <nguyenthanh.5s4d@xf164.onmicrosoft.com>
2022-06-02 02:10:10 +0700

    Create bai0112.c

---
+++ chuong-3/bai0112.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i, j, m, n;
+
+    printf("\nNhap m: ");
+    scanf("%d", &m);
+
+    // Câu a
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= m; ++i)
+    {
+        for(j = 1; j <= n; ++j) 
+        {
+            printf("*");
+        }
+        printf("\n"); //xuong dong khi in xong 1 hang 
+    }
+
+    printf( "\n" );
+
+    for(i = 1; i <= m; ++i)
+    {
+        for(j = 1; j <= n; ++j) 
+        {
+            if( i == 1 || i == m || j == 1 || j == n )
+            {
+                printf("*");
+                if(j == n)
+                printf( "\n" );
+            }
+            else
+                printf(" "); 
+        }
+    }
+    getch();
+    return 0;
+}
