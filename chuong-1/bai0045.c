6d529cda377b7a43feefe0d48376134fa2473160
Nguyễn Thành <nguyenthanh.5s4d@xf164.onmicrosoft.com>
2022-06-01 23:00:26 +0700

    Create bai0045.c

---
+++ chuong-1/bai0045.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    int themang, n;
+    int P;
+
+    do
+    {
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+    }while(n < 0 && printf("\nLoi: n >= 0 !"));
+    P = 1;
+    themang = n;
+
+    while(themang != 0)
+    {
+        P = P * (themang % 10);
+        themang = themang / 10;
+    }
+    printf("\nTich la %d", P);
+
+    getch();
+    return 0;
+}
