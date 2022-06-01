cd36b6757f48c6d59463b9266e6d39cfc5645a23
Nguyễn Thành <nguyenthanh.5s4d@xf164.onmicrosoft.com>
2022-06-02 02:09:45 +0700

    Create bai0111.c

---
+++ chuong-3/bai0111.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i, j, n;
+
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+
+    // Câu a:
+    for(i = 1; i <= n; ++i)
+    {
+        for(j = 1; j <= n + i - 1; ++j)
+        {
+            printf((j < n - i + 1) ? " " : "%c", 234);
+        }
+        putchar('\n');
+    }
+
+
+
+    // Câu b:
+    for(i = 1; i <= n; ++i)
+    {
+        for(j = 1; j <= n + i - 1; ++j)
+        {
+            printf((j == n - i + 1 || j == n + i - 1 || i == n) ? "%c" : " ", 234);
+        }
+        putchar('\n');
+    }
+    printf("\n");
+    // Câu c: 
+    for(i = 1; i <= n; ++i)
+    {
+        for(j = 1; j <= n; ++j)
+        {
+            printf((j <= i) ? "%c" : " ", 234);
+        }
+        printf("\n\n");
+    }
+
+    // Câu d
+    for(i = 1; i <= n; ++i)
+    {
+        for(j = 1; j <= n; ++j)
+        {
+            printf((j == i || j == 1 || i == n) ? "*" : " ");
+        }
+        printf("\n");
+    }
+
+     
+    getch();
+    return 0;
+}
+
+//#include<stdio.h>
+//#include<conio.h>
+//int main()
+//{
+//    for (int i = 0; i < 255; i++)
+//    {
+//        printf("ASCII value of character %c: %d\n", i, i);
+//    }
+//    getch();
+//    return 0;
+//}
