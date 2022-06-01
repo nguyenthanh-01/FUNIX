479c1d494e7c86a012e5b9a7e29175cf93dd72a8
Nguyễn Thành <nguyenthanh.5s4d@xf164.onmicrosoft.com>
2022-06-02 02:40:11 +0700

    Create bai0116.c

---
+++ Hàm/bai0116.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+long tong(int n)
+{
+    long s = 0;
+    int i = 1;
+    while(i <= n)
+    {
+        s = s + i;
+        i++;
+    }
+    return s;
+}
+
+int main()
+{
+    int n;
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+    
+    long ketqua = tong(n);
+    printf("\nTong = %ld", ketqua);
+
+    getch();
+    return 0;
+}
